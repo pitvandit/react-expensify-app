bfd60281c3dc8820970174106c92ff53e6bb553b
Piotr Czarnecki <p.czarnecki@interactive-solutions.com>
2017-11-04 12:49:02 +0100

    setup production webpack build

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Git COMMANDS
+
+git init - create a new git repo
+git status - view the changes to your procject
+git add - Add files to staging area ( git add . -> for all files )
+git commit - create a new commit with files from staging area
+git log - view recent commits
--- src/app.js
+++ src/app.js
@@ -4,15 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
 import AppRouter from './routers/AppRouter';
 
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
+
+import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css';
+import './styles/styles.scss';
+
 
 const store = configureStore();
 
